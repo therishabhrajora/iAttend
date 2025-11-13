482f92210fbf7fd213334cd320f9327ea2ee02c3
therishabhrajora <rishabhrajora20gmail.com>
2025-11-13 23:34:16 +0530

    remove health check

--- backend/src/main/java/com/backend/iAttend/controller/HealthController.java
+++ backend/src/main/java/com/backend/iAttend/controller/HealthController.java
@@ -1,14 +1,14 @@
-package com.backend.iAttend.controller;
+// package com.backend.iAttend.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class HealthController {
+// @RestController
+// public class HealthController {
 
-    @GetMapping("/api/health")
-    public String healthCheck() {
-        return "Server is awake!";
-    }
-}
+//     @GetMapping("/api/health")
+//     public String healthCheck() {
+//         return "Server is awake!";
+//     }
+// }
 
